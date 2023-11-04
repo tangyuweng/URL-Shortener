60226c2f230c999b4bfbdd7bf4560b2439c6b74c
tangyuweng <tangyuw99@gmail.com>
2023-11-04 23:22:33 +0800

    build: use express-handlebars as view engine

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
 
+app.use(express.static('public'))
+app.engine('.hbs', engine({ extname: '.hbs' }))
+app.set('view engine', '.hbs')
+app.set('views', './views')
+
 app.get('/', (req, res) => {
-  res.send('Hello')
+  res.render('index')
 })
 
 app.listen(port, () => {
